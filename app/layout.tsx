f165b8452f5937fe1f880154e72835bc15b321c3
Vishnu Vinayan <therealvishnuvinayan@gmail.com>
2024-07-18 20:20:59 +0400

    themswitcher added in signup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from './providers';
 
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/config/fonts';
+import { ThemeSwitch } from '@/components/theme-switch';
 
 export const metadata: Metadata = {
   title: {
@@ -41,6 +42,10 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
           <div className="relative flex flex-col h-screen">
+            <div className='flex justify-end mt-2 mr-2'>
+            <ThemeSwitch />
+            </div>
+          
             {/* <Navbar /> */}
             <main
             // className="container mx-auto max-w-7xl pt-16 px-6 flex-grow"
